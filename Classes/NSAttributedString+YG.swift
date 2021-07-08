4a6e68b510b13abf5420b74805897c670fb4fe52
zhouyg <zhouyg@feiyu.com>
2021-07-08 09:49:15 +0800

    扩充获取最大值分类

--- Classes/NSAttributedString+YG.swift
+++ Classes/NSAttributedString+YG.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+public extension NSAttributedString {
+
+    /// 根据固定高度获取动态宽度
+    public func yg_getWidthByMaxHeight(_ height: Int) -> CGFloat {
+        return self.yg_size(Int.max, height).width
+    }
+    
+    /// 根据固定宽度获取动态高度
+    public func yg_getHeightByMaxWidth(_ width: Int) -> CGFloat {
+        return self.yg_size(width, Int.max).height
+    }
+
+    /// 根据 宽 / 高, 获取 size
+    public func yg_size(_ width: Int, _ height: Int) -> CGSize {
+        return self.boundingRect(with: CGSize.init(width: width, height: height), options: .usesLineFragmentOrigin, context: nil).size
+    }
+
+}
--- YGRichText_Swift.podspec
+++ YGRichText_Swift.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |spec|
   spec.name         = "YGRichText_Swift"
-  spec.version      = "1.0.1"
+  spec.version      = "1.0.2"
   spec.summary      = "链式设置富文本"
 
   spec.description  = <<-DESC
